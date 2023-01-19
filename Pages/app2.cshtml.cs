323a68bce2a6f4eb36dd6a60579f36de5bc07469
Crescenzo Livia <L.Crescenzo@almaviva.it>
2023-01-19 16:35:42 +0100

    Add project files.

---
+++ Pages/app2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetwebapp.Pages
+{
+    public class app2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/app3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetwebapp.Pages
+{
+    public class app3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
